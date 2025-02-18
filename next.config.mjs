69b8f82f4e867ffbe55054b4b6b2f06155dd26d4
Jon Karkafiris <jonkarkafiris@Jons-MacBook-Pro.local>
2025-02-18 18:31:35 +1030

    Your

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ['*'],
     unoptimized: true
   },
-  trailingSlash: true,
+  trailingSlash: false,
   poweredByHeader: false
 };
 
