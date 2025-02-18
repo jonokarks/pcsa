e45ea1bda08db03c2e056bdd884513a629cf699a
Jon Karkafiris <jonkarkafiris@Jons-MacBook-Pro.local>
2025-02-18 19:18:01 +1030

    please work

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,11 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  poweredByHeader: false
+  poweredByHeader: false,
+  env: {
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: 'pk_test_51QiDnFIpu7s8bD02SNQhAi1aPbKvkhP67B9Ff7Rw4Smv3ZaP6qgGDcRwwcZI6UteAO6B0ibUcdeWRvc8HxbE3tCT00O0LwkPU2',
+    STRIPE_SECRET_KEY: 'sk_test_51QiDnFIpu7s8bD02mnhCE87eAp4T7aQmla2DHUe6LXziDg5gmMMy5HlYf2w6RGp6BmDJhQyTXDzAWqj3w6pgiD9300pDJAysuB'
+  }
 };
 
 export default nextConfig;
