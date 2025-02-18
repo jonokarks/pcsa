32e78a3e9cd6d0c2f36a403c5889f0d4f74bc00c
Jon Karkafiris <jonkarkafiris@Jons-MacBook-Pro.local>
2025-02-18 18:25:36 +1030

    Your com

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
+    domains: ['*'],
     unoptimized: true
   },
   trailingSlash: true,
-  poweredByHeader: false,
-  distDir: 'out',
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false };
-    return config;
-  }
+  poweredByHeader: false
 };
 
 export default nextConfig;
